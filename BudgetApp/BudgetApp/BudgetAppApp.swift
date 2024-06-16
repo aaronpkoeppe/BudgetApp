0699c7c9d33c541a9e21636a30e5630691f28948
aaronpkoeppe <95326718+aaronpkoeppe@users.noreply.github.com>
2024-06-16 12:41:15 -0500

    Add files via upload

---
+++ BudgetApp/BudgetApp/BudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetAppApp.swift
+//  BudgetApp
+//
+//  Created by Aaron Koeppe on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct BudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
